5b37e791c765a50a198ca63357d5b9b540350280
gb-redRabit <136236629+gb-redRabit@users.noreply.github.com>
2025-06-06 22:44:44 +0200

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://quiz-app-rysh.onrender.com";
 
 createApp(App).use(router).use(store).mount("#app");
