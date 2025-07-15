4d8a09464c4582c127b615f4f5e4d424bb2fd5d0
gb-redRabit <136236629+gb-redRabit@users.noreply.github.com>
2025-07-15 23:38:48 +0200

    fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,8 +20,9 @@ app.use(
       "https://quize-nnjdhzbym-grzegorzs-projects-7e242f51.vercel.app",
       "https://quize-git-main-grzegorzs-projects-7e242f51.vercel.app",
       "https://quiz-app-rysh.onrender.com",
-    ], // dodaj tu domenę frontu
+    ],
     credentials: true,
+    exposedHeaders: ["Content-Disposition"], // jeśli eksportujesz pliki
   })
 );
 
@@ -38,29 +39,6 @@ app.use(
     },
   })
 );
-
-// RĘCZNE ustawianie nagłówków CORS dla każdej odpowiedzi
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    "https://quize-nnjdhzbym-grzegorzs-projects-7e242f51.vercel.app",
-    "https://quize-git-main-grzegorzs-projects-7e242f51.vercel.app",
-    "https://quiz-app-rysh.onrender.com",
-    "https://quize-tawny.vercel.app",
-    "http://localhost:8080",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  res.header("Access-Control-Expose-Headers", "Content-Disposition"); // <-- DODAJ TO
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
