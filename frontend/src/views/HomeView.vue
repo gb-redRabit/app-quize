669e74bf8e90b63154d6b42624a2f6c5bbef9acc
gb-redRabit <136236629+gb-redRabit@users.noreply.github.com>
2025-08-22 16:15:21 +0200

    del
     Sprawdź, czy użytkownik widział już instrukcję

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -210,14 +210,6 @@ const gridColumns = ref(3);
 const searchQuery = ref('');
 const showUserGuide = ref(false);
 
-// Sprawdź, czy użytkownik widział już instrukcję
-onMounted(() => {
-  if (!localStorage.getItem('userGuideSeen')) {
-    showUserGuide.value = true;
-    localStorage.setItem('userGuideSeen', '1');
-  }
-});
-
 const categoryStats = computed(() => {
   const stats = {};
   const categories = questionsHelper.categories.value;
